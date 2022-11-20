519262ffe54afafc3b5e04631b83ed955064c20c
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-11-20 13:17:26 -0800

    Change project model and controller to support attachments

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ApplicationRecord
+  
+  has_one_attached :thumbnail do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  has_many_attached :images
+  has_many_attached :files
+
 end
