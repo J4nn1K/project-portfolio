e0f35bd678f451e46b370c602ce8a4c9a4e3c6c1
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-11-23 19:14:38 -0800

    Add year to project params

---
+++ db/migrate/20221121213757_add_date_to_project.rb
@@ -0,0 +1,5 @@
+class AddDateToProject < ActiveRecord::Migration[7.0]
+  def change
+    add_column :projects, :year, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_193857) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_213757) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_193857) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "year"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
